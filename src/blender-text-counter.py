9816c7afdd5e609151c3ff6dadda8873195a4cbd
DFrostt <denfrostt@gmail.com>
2018-12-28 13:46:14 +0400

    Update blender-text-counter.py
    -rework for 2.8
    -Active single-column layout for panel

--- src/blender-text-counter.py
+++ src/blender-text-counter.py
@@ -1,8 +1,8 @@
 bl_info = {
     "name": "Text Counter",
-    "author": "LeoMoon Studios - www.LeoMoon.com and Marcin Zielinski - www.marcin-zielinski.tk/en/",
-    "version": (1, 2, 1),
-    "blender": (2, 6, 9),
+    "author": "LeoMoon Studios - www.LeoMoon.com and Marcin Zielinski - www.marcin-zielinski.tk/en/, Deen",
+    "version": (1, 2, 2),
+    "blender": (2, 80, 0),
     "location": "Font Object Data > Text Counter",
     "description": "Text counter for displays, HUDs etc.",
     "warning": "",
@@ -60,34 +60,34 @@ def formatCounter(input, timeSeparators, timeLeadZeroes, timeTrailZeroes, timeMo
 class TextCounter_Props(bpy.types.PropertyGroup):
     def val_up(self, context):
         textcounter_update_val(context.object, context.scene)
-    ifAnimated = BoolProperty(name='Counter Active', default=False, update=val_up)
-    counter = FloatProperty(name='Counter', update=val_up)
-    padding = IntProperty(name='Padding', update=val_up, min=1)
-    ifDecimal = BoolProperty(name='Decimal', default=False, update=val_up)
-    decimals = IntProperty(name='Decimal', update=val_up, min=0)
-    typeEnum = EnumProperty(items=[('ANIMATED', 'Animated', 'Counter values from f-curves'), ('DYNAMIC', 'Dynamic', 'Counter values from expression')], name='Type', update=val_up, default='ANIMATED')
-    formattingEnum = EnumProperty(items=[('NUMBER', 'Number', 'Counter values as numbers'), ('TIME', 'Time', 'Counter values as time')], name='Formatting Type', update=val_up, default='NUMBER')
-    expr = StringProperty(name='Expression', update=val_up, default='')
-    prefix = StringProperty(name='Prefix', update=val_up, default='')
-    sufix = StringProperty(name='Sufix', update=val_up, default='')
-    ifTextFile = BoolProperty(name='Override with Text File', default=False, update=val_up)
-    textFile = StringProperty(name='Text File', update=val_up, default='')
-    ifTextFormatting = BoolProperty(name='Numerical Formatting', default=False, update=val_up)
-    timeSeparators = IntProperty(name='Separators', update=val_up, min=0, max=3)
-    timeModulo = IntProperty(name='Last Separator Modulo', update=val_up, min=1, default=24)
-    timeLeadZeroes = IntProperty(name='Leading Zeroes', update=val_up, min=1, default=2)
-    timeTrailZeroes = IntProperty(name='Trailing Zeroes', update=val_up, min=1, default=2)
+    ifAnimated : BoolProperty(name='Counter Active', default=False, update=val_up)
+    counter : FloatProperty(name='Counter', update=val_up)
+    padding : IntProperty(name='Padding', update=val_up, min=1)
+    ifDecimal : BoolProperty(name='Decimal', default=False, update=val_up)
+    decimals : IntProperty(name='Decimal', update=val_up, min=0)
+    typeEnum : EnumProperty(items=[('ANIMATED', 'Animated', 'Counter values from f-curves'), ('DYNAMIC', 'Dynamic', 'Counter values from expression')], name='Type', update=val_up, default='ANIMATED')
+    formattingEnum : EnumProperty(items=[('NUMBER', 'Number', 'Counter values as numbers'), ('TIME', 'Time', 'Counter values as time')], name='Formatting Type', update=val_up, default='NUMBER')
+    expr : StringProperty(name='Expression', update=val_up, default='')
+    prefix : StringProperty(name='Prefix', update=val_up, default='')
+    sufix : StringProperty(name='Sufix', update=val_up, default='')
+    ifTextFile : BoolProperty(name='Override with Text File', default=False, update=val_up)
+    textFile : StringProperty(name='Text File', update=val_up, default='')
+    ifTextFormatting : BoolProperty(name='Numerical Formatting', default=False, update=val_up)
+    timeSeparators : IntProperty(name='Separators', update=val_up, min=0, max=3)
+    timeModulo : IntProperty(name='Last Separator Modulo', update=val_up, min=1, default=24)
+    timeLeadZeroes : IntProperty(name='Leading Zeroes', update=val_up, min=1, default=2)
+    timeTrailZeroes : IntProperty(name='Trailing Zeroes', update=val_up, min=1, default=2)
 
     # newly added
-    useCommas = BoolProperty(
+    useCommas : BoolProperty(
         name='Commas', default=False, update=val_up,
         description='Use commas in large numbers')
-    useDecimalComma = BoolProperty(
+    useDecimalComma : BoolProperty(
         name='Swap . ,', default=False, update=val_up,
         description='Use commas in place of decimals')
-    useAbbreviation = BoolProperty(name='Abbreviate', default=False, update=val_up,
+    useAbbreviation : BoolProperty(name='Abbreviate', default=False, update=val_up,
         description='Use large or small number abbreviations')
-    useAbbreviationLower = BoolProperty(name='Lowercase', default=False, update=val_up,
+    useAbbreviationLower : BoolProperty(name='Lowercase', default=False, update=val_up,
         description='Use lowercase abbreviations')
 
     def dyn_get(self):
@@ -98,7 +98,7 @@ class TextCounter_Props(bpy.types.PropertyGroup):
             return str(eval(self.expr))
         except Exception as e:
             print('Expr Error: '+str(e.args))
-    dynamicCounter = StringProperty(name='Dynamic Counter', get=dyn_get, default='')
+    dynamicCounter : StringProperty(name='Dynamic Counter', get=dyn_get, default='')
     
     def form_up(self, context):
         textcounter_update_val(context.object, context.scene)
@@ -124,11 +124,11 @@ class TextCounter_Props(bpy.types.PropertyGroup):
         self.counter = float(counter)
         
 
-    formattedCounter = StringProperty(name='Formatted Counter', get=form_get, set=form_set, default='')
+    formattedCounter : StringProperty(name='Formatted Counter', get=form_get, set=form_set, default='')
     
 
 #
-class TextCounterPanel(bpy.types.Panel):
+class TextCounter_PT_Panel(bpy.types.Panel):
     """Creates a Panel in the Font properties window"""
     bl_label = "Text Counter"
     bl_idname = "text_counter_panel"
@@ -136,18 +136,20 @@ class TextCounterPanel(bpy.types.Panel):
     bl_region_type = 'WINDOW'
     bl_context = "data"
 
-    counter = FloatProperty(name='Counter')
+    counter : FloatProperty(name='Counter')
     @classmethod
     def poll(cls, context):
         return context.object.type == 'FONT'
 
     def draw_header(self, context):
-        self.layout.prop(context.object.data.text_counter_props, 'ifAnimated', '')
+        self.layout.prop(context.object.data.text_counter_props, 'ifAnimated', text=' ')
 
     def draw(self, context):
+        
         props = context.object.data.text_counter_props
 
         layout = self.layout
+        layout.use_property_split = True # Active single-column layout
         layout.enabled = props.ifAnimated
         row = layout.row()
         row.prop(props, 'typeEnum', expand=True)
@@ -166,20 +168,21 @@ class TextCounterPanel(bpy.types.Panel):
         row = col.row(align=True)
         row.prop(props, 'formattingEnum', expand=True)
         if props.formattingEnum == 'NUMBER':
-            row = col.row(align=True)
+            
+            row = col.column()
             row.prop(props, 'ifDecimal')
             row.prop(props, 'padding')
             row.prop(props, 'decimals')
-            row = col.row(align=True)
+           
             row.prop(props, 'prefix')
             row.prop(props, 'sufix')
-            row = col.row(align=True)
+           
             colsub = row.column()
             colsub.prop(props, 'useCommas')
             colsub = row.column()
             colsub.enabled = props.useCommas
             colsub.prop(props, 'useDecimalComma')
-            row = col.row(align=True)
+           
             colsub = row.column()
             colsub.prop(props, 'useAbbreviation')
             colsub = row.column()
@@ -187,30 +190,30 @@ class TextCounterPanel(bpy.types.Panel):
             colsub.prop(props, 'useAbbreviationLower')
 
         elif props.formattingEnum == 'TIME':
-            row = col.row(align=True)
+            
+            row = col.column()
+            row = row.column()
             row.prop(props, 'formattedCounter')
-            row = col.row(align=False)
+            
+            row = row.column()
             row.prop(props, 'timeSeparators')
             row.prop(props, 'timeModulo')
-            row = col.row(align=True)
+            row = row.column()
             row.prop(props, 'timeLeadZeroes')
             row.prop(props, 'timeTrailZeroes')
-            row = col.row(align=True)
+            row = row.column()
             row.prop(props, 'prefix')
             row.prop(props, 'sufix')
         
+        
         boxy = layout.box() 
-        row = boxy.row()
         row.prop(props, 'ifTextFile')        
-        row = boxy.row()
+        row = boxy.column()
         row.prop(props, "ifTextFormatting")
         row.prop_search(props, "textFile", bpy.data, "texts", text="Text File")
         if not props.ifTextFile:
             row.enabled = False
             
-        #boxy = layout.box() 
-        #row = boxy.row()
-        #row.prop(props, 'ifTextFile')
             
         
 
@@ -327,16 +330,38 @@ def textcounter_text_update_frame(scene):
         if text.type == 'FONT' and text.data.text_counter_props.ifAnimated:
             textcounter_update_val(text, scene)
 
+classes = (
+    TextCounter_PT_Panel,
+    TextCounter_Props,
+)
+def register():
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+
+    bpy.types.TextCurve.text_counter_props = PointerProperty(type = TextCounter_Props)
+    bpy.app.handlers.frame_change_post.append(textcounter_text_update_frame)
+
+def unregister():
+    bpy.app.handlers.frame_change_post.append(textcounter_text_update_frame)
+    bpy.types.TextCurve.text_counter_props = PointerProperty(type = TextCounter_Props)
+
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
+
+    
+
+if __name__ == "__main__":
+    register()
+""" 
 def register():
     bpy.utils.register_class(TextCounter_Props)
     bpy.types.TextCurve.text_counter_props = PointerProperty(type = TextCounter_Props)
-    bpy.utils.register_class(TextCounterPanel)
+    bpy.utils.register_class(TextCounter_PT_Panel)
     bpy.app.handlers.frame_change_post.append(textcounter_text_update_frame)
 
 def unregister():
     bpy.utils.unregister_class(TextCounter_Props)
-    bpy.utils.unregister_class(TextCounterPanel)
-    bpy.app.handlers.frame_change_post.remove(textcounter_text_update_frame)
-
-if __name__ == "__main__":
-    register()
+    bpy.utils.unregister_class(TextCounter_PT_Panel)
+    bpy.app.handlers.frame_change_post.remove(textcounter_text_update_frame) """
